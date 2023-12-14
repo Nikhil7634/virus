f2266681f64f6386b617a0cd9f1714e3924ac601
nikhil7634 <paln8n7634@gmail.com>
2023-12-14 16:11:33 +0530

    done

---
+++ prac1.py
@@ -0,0 +1,59 @@
+from queue import Queue
+romaniaMap = {
+    'Arad': ['Sibiu', 'Zerind', 'Timisoara'],
+    'Zerind': ['Arad', 'Oradea'],
+    'Oradea': ['Zerind', 'Sibiu'],
+    'Sibiu': ['Arad', 'Oradea', 'Fagaras', 'Rimnicu'],
+    'Timisoara': ['Arad', 'Lugoj'],
+    'Lugoj': ['Timisoara', 'Mehadia'],
+    'Mehadia': ['Lugoj', 'Drobeta'],
+    'Drobeta': ['Mehadia', 'Craiova'],
+    'Craiova': ['Drobeta', 'Rimnicu', 'Pitesti'],
+    'Rimnicu': ['Sibiu', 'Craiova', 'Pitesti'],
+    'Fagaras': ['Sibiu', 'Bucharest'],
+    'Pitesti': ['Rimnicu', 'Craiova', 'Bucharest'],
+    'Bucharest': ['Fagaras', 'Pitesti', 'Giurgiu', 'Urziceni'],
+    'Giurgiu': ['Bucharest'],
+    'Urziceni': ['Bucharest', 'Vaslui', 'Hirsova'],
+    'Hirsova': ['Urziceni', 'Eforie'],
+    'Eforie': ['Hirsova'],
+    'Vaslui': ['Iasi', 'Urziceni'],
+    'Iasi': ['Vaslui', 'Neamt'],
+    'Neamt': ['Iasi']
+}
+def bfs(start, destination):
+    visited, parent, distance = {}, {}, {}
+    bfs_output = []
+    queue = Queue()
+
+    for city in romaniaMap.keys():
+        visited[city] = False
+        parent[city] = None
+        distance[city] = -1
+
+    startingCity = start
+    visited[startingCity] = True
+    distance[startingCity] = 0
+    queue.put(startingCity)
+
+    while not queue.empty():
+        u = queue.get()
+        bfs_output.append(u)
+
+        for v in romaniaMap[u]:
+            if not visited[v]:
+                visited[v] = True
+                parent[v] = u
+                distance[v] = distance[u] + 1
+                queue.put(v)
+
+    g = destination
+    path = []
+    while g is not None:
+        path.append(g)
+        g = parent[g]
+
+    path.reverse()
+    print(path)
+
+bfs('Arad', 'Bucharest')
