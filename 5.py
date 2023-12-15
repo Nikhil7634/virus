9dea92c9f47f34be1addba2089867fd2a9857b34
nikhil7634 <paln8n7634@gmail.com>
2023-12-16 01:39:24 +0530

    done

--- 5.py
+++ 5.py
@@ -1,16 +1,14 @@
-from random import randint
-if __name__ == '__main__':
-    P = 23
-    G = 9
-    print('The Value of P is: %d' % P)
-    print('The Value of G is: %d' % G)
-    a = 4
-    print('Secret Number for Alice is: %d' % a)
-    x = pow(G, a, P)
-    b = 6
-    print('Secret Number for Bob is: %d' % b)
-    y = pow(G, b, P)
-    ka = pow(y, a, P)
-    kb = pow(x, b, P)
-    print('Secret Key for Alice is: %d' % ka)
-    print('Secret Key for Bob is: %d' % kb)
+import pandas
+from sklearn import model_selection
+from sklearn.ensemble import AdaBoostClassifier
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indiansdiabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_csv(url, names=names)
+array = dataframe.values
+X = array[:, 0:8]
+Y = array[:, 8]
+seed = 7
+num_trees = 30
+model = AdaBoostClassifier(n_estimators=num_trees, random_state=seed)
+results = model_selection.cross_val_score(model, X, Y)
+print(results.mean())
